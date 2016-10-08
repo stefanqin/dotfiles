88daf3dbe68f251bc3ed968c020ba11e49325eea
Stefan Qin <stefan_qin@yahoo.com>
2016-10-07 22:43:45 -0700

    Fixed symlink to make more portable

--- symlink_all.sh
+++ symlink_all.sh
@@ -2,7 +2,7 @@
 # NOTE: Please run this file in the root directory (cd ~)
 # This file symlinks all the existing dot to root.
 
-for dotfile in ~/Dotfiles/.*; do
+for dotfile in $HOME/dotfiles/.*; do
     # Match only the filename part of a variable.
     file_name="$( echo "$dotfile"|grep -o "\..*" )"
     ln -sv "$dotfile" "$HOME/$file_name"
